def7eddfadeeaabdad91bf0b91399251610cf7fd
nanamesua <nanamesua@gmail.com>
2022-09-13 06:37:37 -0700

    c nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+	char *a = "_putchar";
+
+	while (*a)
+	{
+		_putchar(*a);
+		a++;
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+
+}
