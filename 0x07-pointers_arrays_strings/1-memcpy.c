84f80ac2726697778f902b8e5b122904c3e66aeb
nanamesua <nanamesua@gmail.com>
2022-09-26 07:19:32 -0700

    pointers,arrays,strings,2d arrays

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memcpy - function copies n bytes from memory area src to memory area dest
+ *@src: memory area source to copy from
+ *@dest: memory area destination to copy
+ *@n: number of bytes
+ *Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	char *temp = *dest;
+
+	while (n--)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	return (temp);
+}
