c80caf12fe6993c0f13c9645df970e7b84e63183
nanamesua <nanamesua@gmail.com>
2022-10-04 10:14:15 -0700

    dynamic memory allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,21 +11,30 @@
 int **alloc_grid(int width, int height)
 
 {
-	int **arr, i = 0, j = 0;
+	int **arr, i, j;
+	int len = width * height;
 
-	if (width <= 0 || height <= 0)
+	if (len <= 0)
 		return (NULL);
+
 	arr = (int **)malloc(sizeof(int *) * height);
 	if (arr == NULL)
 		return (NULL);
-	for (; i < height; i++)
+
+	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(arr[i]);
+			free(arr);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
-		for (; j < width; j++)
+		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
+
 	return (arr);
 }
