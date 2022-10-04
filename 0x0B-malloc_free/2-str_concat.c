9bf56f67979a3a36fa303042e901e4d4d939526c
nanamesua <nanamesua@gmail.com>
2022-10-04 09:43:25 -0700

    dynamic memory allocation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,48 +10,33 @@
 char *str_concat(char *s1, char *s2)
 
 {
-	char *new_str, *string1, *string2;
+	int w, x, len1, len2, len;
+	char *string;
 
-	int i = 0, len_s1 = 0, len_s2 = 0;
+	len1 = len2 = 0;
 
-	string1 = s1;
-	string2 = s2;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	while (*s1)
+	if (s1 != NULL)
 	{
-		len_s1++;
-		s1++;
+		w = 0;
+		while (s1[w++] != '\0')
+			len1++;
 	}
-	s1 = string1;
-	if (s2 == NULL)
-		s2 = "";
-	while (*s2)
+	if (s2 != NULL)
 	{
-		len_s2++;
-		s2++;
+		w = 0;
+		while (s2[w++] != '\0')
+			len2++;
 	}
-	s2 = string2;
-	new_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
-	string1 = new_str;
-	if (new_str == NULL)
+
+	len = len1 + len2;
+	string = (char *)malloc(sizeof(char) * (len + 1));
+	if (string == NULL)
 		return (NULL);
-	for (; i < (len_s1 + len_s2); i++)
-	{
-		if (i < len_s1)
-		{
-			new_str[i] = *s1;
-			s1++;
-		}
-		else
-		{
-			new_str[i] = *s2;
-			s2++;
-		}
-	}
-	new_str[i] = '\0';
-	return (string1);
-}
+	for (w = 0; w < len1; w++)
+		string[w] = s1[w];
+	for (x = 0; x < len2; x++, w++)
+		string[w] = s2[x];
+	string[len] = '\0';
 
+	return (string);
+}
