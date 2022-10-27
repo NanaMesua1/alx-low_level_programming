f21ab9a8497c230bf6c7596d7fa0ef84e3a58492
nanamesua <nanamesua@gmail.com>
2022-10-27 09:53:46 -0700

    C bit manipulations

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,11 +8,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int i;
 
-	max <<= index;
-	if (max == 0)
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	*n |= max;
+	i = 1 << index;
+	*n = *n | i;
+
 	return (1);
 }
