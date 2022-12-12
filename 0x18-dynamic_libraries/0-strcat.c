02448d12225cd834be37569522b0e0623a605959
NanaMesua1 <dorindant17@gmail.com>
2022-12-12 04:34:02 -0800

    dynamis libraries in c

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcat - Concatenates two strings
+ *@dest: Destination string
+ *@src: Source string
+ *Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+
+{
+	int dest_len = 0;
+	int src_len = 0;
+
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[src_len])
+	{
+		dest[dest_len] = src[src_len];
+		src_len++;
+		dest_len++;
+	}
+	dest[dest_len] = '\0';
+
+	return (dest);
+}
